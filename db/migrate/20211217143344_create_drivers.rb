f4479d4dffdfe35522f551bf419fe0e50b2946de
ShahendaKh <shahenda.khaled@hotmail.com>
2021-12-18 04:07:29 +0200

    [GAL1] Add signup action fr drivers and car owners

--- db/migrate/20211217143344_create_drivers.rb
+++ db/migrate/20211217143344_create_drivers.rb
@@ -2,8 +2,6 @@ class CreateDrivers < ActiveRecord::Migration[6.1]
   def change
     create_table :drivers do |t|
       t.string :license_image
-      t.references :user, foreign_key: true
-      
       t.timestamps
     end
   end
